a040002608ee0b38057449161e751e8f8904513e
AlekseySav <alex2003s@yandex.ru>
2023-06-24 02:22:44 +0300

    upd sign

--- src/lp/lp.py
+++ src/lp/lp.py
@@ -1,5 +1,6 @@
 from fractions import Fraction
 from dataclasses import dataclass
+import operator as op
 
 
 @dataclass
@@ -15,31 +16,42 @@ Column = list[Fraction]
 Matrix = list[list[Fraction]]
 
 
-def normalize(constraints: Matrix, objectives: Column, vars: list[Variable]) -> None:
+def normalize(constraints: Matrix, bounds: Column, objectives: Column, vars: list[Variable]) -> None:
     assert len(objectives) == len(constraints[0])
-
     n = len(vars)
     m = len(objectives)
 
-    # fix bounds
+    # make variables positive, unbounded
+    def add_bound(bound, op, a, b):
+        if bound is None:
+            return
+        if op(bound, 0):
+            vars[a].displacement = bound
+            constraints.append([0 if i != b else 1 for i in range(len(vars))])
+            bounds.append(0)
+        else:
+            constraints.append([0 if i != b else 1 for i in range(len(vars))])
+            bounds.append(-bound)
+
     for i in range(n):
         # x_{i} - x_{i+n} is original x_{i}
-        vars.append(Variable(vars[i].is_int))
-        pos, neg = vars[i], vars[i + n]
-        if pos.lower_bound is not None:
-            pos.displacement = pos.lower_bound
-        if pos.upper_bound is not None:
-            neg.displacement = -pos.upper_bound
-        pos.lower_bound = pos.upper_bound = None
+        vars.append(Variable(vars[i].is_int, 0))
+        v = vars[i]
+        add_bound(v.lower_bound, op.gt, i, n + i)
+        add_bound(v.upper_bound, op.lt, i, n + i)
+        v.upper_bound = None
+        v.lower_bound = 0
 
     # constraints -> equations
     equ = []
-    for i, line in enumerate(constraints):
+    for id, line in enumerate(constraints):
         equ.append([
-                line[i] if i < m else
+                line[i] if i < len(line) else
                 -line[i - m] if i < 2 * m else
-                0
-                for i in range(len(vars))] + [0]
+                0 if i != id + 2 * m else
+                1
+                for i in range(2 * m + len(constraints))
+            ]
             )
         vars.append(Variable(False))
     constraints[:] = equ[:]
@@ -57,3 +69,23 @@ def evaluate(vars: list[Variable], n: int) -> list[Fraction]:
     return res
 
 
+def nice_print(M, B, O, V):
+    for m, b in zip(M, B):
+        print(' + '.join(f'{i}*x{ii}' for ii, i in enumerate(m)), f'<= {b}')
+    print('max: ', ' + '.join(f'{i}*x{ii}' for ii, i in enumerate(O)))
+    print('where: ', ', '.join(f'{v.lower_bound} <= x{i}-{v.displacement}({"int" if v.is_int else "real"}) <= {v.upper_bound}'
+        for i, v in enumerate(V)))
+
+# M = [
+#     [1, 2, 3]
+# ]
+
+# B = [6]
+
+# O = [1, 9, 0]
+
+# V = [Variable(False, 2, 5), Variable(False), Variable(True)]
+
+# nice_print(M, B, O, V)
+# normalize(M, B, O, V)
+# nice_print(M, B, O, V)
