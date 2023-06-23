bc23e6ff76376eebc60da97a7388351818e7d7e8
sergiy <iarygin.sv@phystech.edu>
2023-06-23 20:07:55 +0300

    added rules

---
+++ src/milp/milp.py
@@ -0,0 +1,17 @@
+class Solver:
+    def __init__(self, name=""):
+        self.name = name
+
+    def IntVar(self, lower_bound, upper_bound, name):
+        # self.vars[name] = newVar()
+        return ...
+
+    def
+
+    def Add(self):
+        return ...
+
+
+
+def CreateSolver(name=""):
+    return Solver(name)
