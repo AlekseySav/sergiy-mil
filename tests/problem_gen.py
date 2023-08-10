d4a023125780399ee6a27f3890f10b4d4e9b920e
Sergiy Yarygin <iarygin.sv@phystech.edu>
2023-08-10 13:22:12 +0300

    Revert "extra tests"
    This reverts commit eb5a18605af92086f1f7253511eb3a988771f452.

---
+++ tests/problem_gen.py
@@ -0,0 +1,38 @@
+import math
+
+from tests.problem_api import Problem
+from tests.value_gen import ValueGenerator
+
+
+class ProblemGenerator:
+    generators: dict[str:ValueGenerator]
+    vars: int
+    cons: int
+
+    def __init__(self, modes: dict[str:str],
+                 values: dict[str: float | list[float]],
+                 variables_count: int,
+                 constraints_count: int):
+        self.vars = variables_count
+        self.cons = constraints_count
+        self.generators = {}
+        for key in modes.keys():
+            if key not in values.keys():
+                self.generators[key] = ValueGenerator(mode=modes[key])
+            if isinstance(values[key], float):
+                self.generators[key] = ValueGenerator(mode=modes[key], E=values[key])
+            if isinstance(values[key], list):
+                self.generators[key] = ValueGenerator(mode=modes[key], E=values[key][0], D=values[key][1])
+
+    def value(self) -> Problem:
+        constraints_coeffs = [
+            [math.floor(self.generators['constraints_coeffs'].value()) for _ in range(self.vars)]
+            for _ in range(self.cons)
+        ]
+        bounds = [
+            math.floor(self.generators['bounds'].value()) for _ in range(self.cons)
+        ]
+        obj_coeffs = [
+            math.floor(self.generators['obj_coeffs'].value()) for _ in range(self.vars)
+        ]
+        return Problem(constraints_coeffs, bounds, obj_coeffs)
