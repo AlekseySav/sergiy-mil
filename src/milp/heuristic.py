f5ae811f15b14ab5183ebf014f6e558cfd9d6005
sergiy <iarygin.sv@phystech.edu>
2023-09-12 14:30:58 +0300

    float32->float64

--- src/milp/heuristic.py
+++ src/milp/heuristic.py
@@ -1,14 +1,16 @@
 from collections.abc import Callable
 
-from src.tableau import Tableau
+from numpy import argmax
 
+from lp import solve_lp
+from src.tableau import Tableau, array
 
 func_get_tableau = Callable[[list[Tableau], list[bool]], Tableau | None]
 func_get_axis = Callable[[Tableau, list[bool]], int | None]
 
 
 def gt_simple(problems: list[Tableau], axis: list[bool]) -> Tableau | None:
-    if len(problems) > 0:
+    if problems:
         return problems[0]
     return None
 
@@ -18,3 +20,16 @@ def ga_simple(problem: Tableau, in_constraints: list[bool]) -> int | None:
         if not val:
             return i
     return None
+
+
+def gt_fifo(problems: list[Tableau], axis: list[bool]) -> Tableau | None:
+    if problems:
+        return problems[-1]
+    return None
+
+
+def gt_max_solution(problems: list[Tableau], axis: list[bool]) -> Tableau | None:
+    if not problems:
+        return None
+    z = [p.solution() if solve_lp(p) else float('-inf') for p in problems]
+    return problems[argmax(array(z))]
